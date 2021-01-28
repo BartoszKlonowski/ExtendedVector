dead17658cdc14e86a1258e5823a0a0fa97e3609
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2021-01-28 19:33:42 +0100

    Project structure complete redesign - Examples, Source and Tests - configuration (#4)
    * Convert Source/ into shared files project directory
    
    The Source/ directory contains the main source of the SharpVector
    implementation: Vector.hpp and Vector.cpp (used only to compile the
    header only).
    It was based on CMake placed in this directory which was causing the
    whole project to contain one configuration with multiple subconfigured
    parts of the project.
    
    To make the project structure clean, by seperating each component of the
    project, the Source/ directory has been converted into the project
    itself with it's own VS configuration.
    
    The Source project is a Shared Items Project
    which allows to keep the files simple, but to share them between other
    components.
    
    * Change Source.vcxitems to Source.vcxproj
    
    The Shared Items Project used for the main source of SharpVector
    implementation had the only disadvantage:
    It was impossible to build it on it's own.
    But building the SharpVector implementation only is crucial for ensuring
    the stability of the whole project and the development process.
    
    To let it be built as a standalone project, the Source.vcxitems has been
    converted into the static library project: Source.vcxproj
    
    * Remove unused legacy SharpVector project
    
    The general SharpVector.vcxproj has currently been unused and was
    already replaced with more specific project.
    It was also removed in previous commit from .sln configuration file.
    
    * Create Tests directory for CppUnitTestFramework
    
    The CppUnitTestFramework will now be used for the unit testing of the
    project (both source and examples).
    The Tests/ directory contains the project configuration and the
    scaffolded Test.cpp implementation file which preents the most basic
    example of unit test implementation.
    
    Test/ directory (previously containing the unit tests) has now been
    renamed to Test-old/ to mark it as a legacy, but to leave it so the
    implementation is not lost.
    
    * Convert App into Examples project
    
    The Examples project is responsible for presenting many features and
    functionality of the SharpVector.
    The idea is to have many single application which will be able to be ran
    from the main() method of the Examples.cpp file.
    
    All this should be possible using many source file of each application
    implementation, but having its instance in the main() method guarded by
    the preprocessor macro set by the compilation configuration.

---
+++ Tests/Tests.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Tests
+{
+	TEST_CLASS(Tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
