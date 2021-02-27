ffae0fb6c7c3fd59736ad48e83a1220f48f7dc01
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2021-02-27 03:32:23 +0100

    Implement CitiesFilter example application 1

--- Examples/Source.cpp
+++ Examples/Source.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+#include "Examples/CitiesFilteringApplication.cpp"
+
+
 int main()
 {
     std::cout << "SharpVector - Examples application!" << std::endl;
-    return 0;
+
+    Examples::ExampleApplication1 example1;
+
+    return static_cast<int>(example1.Run());
 }
