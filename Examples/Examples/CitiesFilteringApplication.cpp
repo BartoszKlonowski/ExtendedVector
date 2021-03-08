2556c7deabd3f5329a688831cefedecbbb5496d0
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2021-03-08 12:24:12 +0100

    Examples project - implementation (#13)
    * Introduce error codes for all application as a common interface
    
    * Implement CitiesFilter example application 1
    
    * Add logs to the ExampleApplication1::Run()
    
    * Implement coordinates example
    
    * Implement BusinessObject example
    
    So the SharpVector implements all the features from the List<T>, it
    seems logical that there should be the same example implemented as it is
    for the original .NET's implementation:
    https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=net-5.0#examples
    
    This example is different from the previous examples, as it doesn't
    check for the correctness of the application run, but instead, it prints
    all the results and information and the SharpVector work to the console.

---
+++ Examples/Examples/CitiesFilteringApplication.cpp
@@ -0,0 +1,140 @@
+#include "../ErrorCodes.hpp"
+#include "../../Source/Vector.hpp"
+#include <string>
+#include <iostream>
+
+
+namespace Examples
+{
+    class City
+    {
+    public:
+        City() = default;
+
+        City( const std::string& name, const unsigned int populationCount ) : name{name}, populationCount{populationCount}
+        {
+        }
+
+        bool operator==( const City& left ) const
+        {
+            return left.name == this->name && left.populationCount == this->populationCount;
+        }
+
+        bool operator<( const City& left ) const
+        {
+            return left.populationCount < this->populationCount;
+        }
+
+        const unsigned int PopulationCount() const noexcept
+        {
+            return populationCount;
+        }
+
+        const std::string Name() const noexcept
+        {
+            return name;
+        }
+
+    private:
+        std::string name;
+        unsigned int populationCount;
+    };
+
+
+    class CitiesFilter
+    {
+    public:
+        CitiesFilter( std::initializer_list<City> data )
+        {
+            cities.AddRange( data );
+        }
+
+        void AddNewCity( const City& newCity )
+        {
+            cities.push_back( newCity );
+        }
+
+        void RemoveCity( City cityToRemove )
+        {
+            cities.Remove( cityToRemove );
+        }
+
+        std::string GetNameOfMostPopulated() noexcept
+        {
+            auto sortedCities = cities;
+            sortedCities.Sort();
+            return sortedCities[0].Name();
+        }
+
+        City FindByName( std::string name )
+        {
+            return cities.Find( [=]( City c )->bool
+                {
+                    return c.Name() == name;
+                } );
+        }
+
+    private:
+        Cx::Vector<City> cities;
+    };
+
+
+    class ExampleApplication1
+    {
+    public:
+        ErrorCodes Run()
+        {
+            auto citiesFilter = CitiesFilter(
+                {
+                    City( "Warsaw", 10000 ),
+                    City( "New York", 2344000 ),
+                    City( "Miami", 2306043 ),
+                    City( "Philadelfia", 3434322 ),
+                    City( "Wroclaw", 899000 ),
+                    City( "Bydgoszcz", 355000 ),
+                    City( "Moscow", 1770100 ),
+                    City( "Paris", 1055799 ),
+                    City( "Madrid", 4999000 ),
+                    City( "Tokyo", 6799899 )
+                } );
+
+            if( citiesFilter.GetNameOfMostPopulated() != "Tokyo" )
+            {
+                std::cout << "ExampleApplication1 - GetNameOfMostPopulated() failed!" << std::endl;
+                return ErrorCodes::Error;
+            }
+
+            const auto cityToRemove = City( "Bydgoszcz", 355000 );
+            citiesFilter.RemoveCity( cityToRemove );
+            if( citiesFilter.FindByName( "Bydgoszcz" ).Name() != "" )
+            {
+                std::cout << "ExampleApplication1 - RemoveCity( \"Bydgosz\", 355000) failed!" << std::endl;
+                return ErrorCodes::Error;
+            }
+
+            citiesFilter.AddNewCity( City( "Krakow", 456098 ) );
+            if( citiesFilter.FindByName( "Krakow" ).PopulationCount() != 456098 )
+            {
+                std::cout << "ExampleApplication1 - AddNewCity( \"Krakow\", 456098) failed!" << std::endl;
+                return ErrorCodes::Error;
+            }
+
+            citiesFilter.RemoveCity( City( "Tokyo", 6799899 ) );
+            if( citiesFilter.FindByName( "Tokyo" ).Name() == "" )
+            {
+                if( citiesFilter.GetNameOfMostPopulated() != "Madrid" )
+                {
+                    std::cout << "ExampleApplication1 - GetNameOfMostPopulated() failed!" << std::endl;
+                    return ErrorCodes::Error;
+                }
+            }
+            else
+            {
+                std::cout << "ExampleApplication1 - FindByName(\"Tokyo\") failed!" << std::endl;
+                return ErrorCodes::Error;
+            }
+
+            return ErrorCodes::Success;
+        }
+    };
+}
--- Examples/Source.cpp
+++ Examples/Source.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+#include "Examples/CitiesFilteringApplication.cpp"
+#include "Examples/Coordinates.cpp"
+#include "Examples/BusinessObject.cpp"
+
+
 int main()
 {
     std::cout << "SharpVector - Examples application!" << std::endl;
-    return 0;
+
+    Examples::ExampleApplication1 example1;
+    Examples::ExampleApplication2 example2;
+    Examples::BusinessObjectExample example3;
+
+    auto result = static_cast<int>(example1.Run()) + static_cast<int>(example2.Run()) + static_cast<int>(example3.Run());
+
+    return result;
 }
